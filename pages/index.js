f134bf9ec46f336be87f67028b5570eb3c473a17
Leticia Cristina <letici4cr1s@gmail.com>
2024-03-13 01:43:34 -0300

    interface front-end ok

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,60 @@
-import { ScrollView, View, Text, StyleSheet, Image } from "react-native";
+import { ScrollView, View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from '@expo/vector-icons/'
 
 export function Home() {
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <ScrollView>
-            <View style={ESTILO.header}>
-                <View style={ESTILO.vita}>
-                    <Image source={require("../assets/logo.png")} style={ESTILO.logo} />
-                    <Text style={ESTILO.titulo}>
-                        VitaMental
+                <View style={ESTILO.header}>
+                    <View style={ESTILO.vita}>
+                        <Image source={require("../assets/logo.png")} style={ESTILO.logo} />
+                        <Text style={ESTILO.titulo}>
+                            VitaMental
+                        </Text>
+                    </View>
+                    <Text style={ESTILO.subtitulo}>
+                        Vitalidade é fundamental.
                     </Text>
+                    <View style={ESTILO.interImg}>
+                        <Image source={require("../assets/mulherQuebraCabeca.png")} style={ESTILO.imgInter} />
+                    </View>
                 </View>
-                <Text style={ESTILO.subtitulo}>
-                    Vitalidade é fundamental.
-                </Text>
-                <View style={ESTILO.interImg}>
-                    <Image source={require("../assets/mulherQuebraCabeca.png")} style={ESTILO.imgInter} />
+                <View style={ESTILO.butoes}>
+                    <TouchableOpacity style={ESTILO.botaoExercicioJogo}>
+                        <View style={ESTILO.exercicioJogo}>
+                            <Text style={ESTILO.tituloBotao}>
+                                Exercícios
+                            </Text>
+                            <Ionicons name="arrow-up-circle-outline" style={ESTILO.iconeBotao} />
+                        </View>
+                        <Text style={ESTILO.textoBotao}>
+                            Pratique sempre os exercícios indicados para fortalecer os ossos.
+                        </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={ESTILO.botaoExercicioJogo}>
+                        <View style={ESTILO.exercicioJogo}>
+                            <Text style={ESTILO.tituloBotao}>
+                                Jogos
+                            </Text>
+                            <Ionicons name="arrow-up-circle-outline" style={ESTILO.iconeBotao} />
+                        </View>
+                        <Text style={ESTILO.textoBotao}>
+                            Pratique sempre os jogos fornecidos para estimular a mente.
+                        </Text>
+                    </TouchableOpacity>
                 </View>
-            </View>
-            <View>
-                button
-            </View>
+                <View style={ESTILO.conhecaMais}>
+                    <TouchableOpacity style={ESTILO.botaoConhecaMais}>
+                        <Text style={ESTILO.tituloConhecaMais}>
+                            Conheça +
+                        </Text>
+                        <Text style={ESTILO.textoConhecaMais}>
+                            Sobre doenças que afetam o sistema nervoso de muitos idosos, resultando em dificuldades cognitivas.
+                        </Text>
+                    </TouchableOpacity>
+                </View>
             </ScrollView>
         </SafeAreaView>
     )
@@ -63,12 +96,68 @@ const ESTILO = StyleSheet.create({
     },
     interImg: {
         paddingTop: 20,
-        alignItems:'center'
+        alignItems: 'center'
     },
     imgInter: {
         height: 250,
         width: 250
+    },
+    butoes: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+    botaoExercicioJogo: {
+        marginTop: 50,
+        width: 150,
+        height: 150,
+        borderRadius: 35,
+        padding: 18,
+        backgroundColor: '#3C4146',
+        justifyContent: 'center',
+        cursor: 'pointer'
+    },
+    exercicioJogo: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+    tituloBotao: {
+        fontSize: 18,
+        color: '#fff'
+    },
+    textoBotao: {
+        fontSize: 12,
+        textAlign: 'center',
+        marginTop: 10,
+        color: '#ffffffe8'
+    },
+    iconeBotao: {
+        fontSize: 30,
+        color: '#fff',
+        transform: [{ rotate: '45rad' }],
+        width: 20,
+        marginTop: -8
+    },
+    conhecaMais: {
+        alignItems: 'center',
+        marginTop: 50,
+        height: 175
+    },
+    botaoConhecaMais: {
+        backgroundColor: '#3C4146',
+        width: 310,
+        height: 130,
+        borderRadius: 35,
+        padding: 20
+    },
+    tituloConhecaMais: {
+        color: '#fff',
+        fontSize: 20,
+        marginBottom: 10
+    },
+    textoConhecaMais: {
+        color: '#ffffffe8',
+        fontSize: 14
     }
-
 
 })
