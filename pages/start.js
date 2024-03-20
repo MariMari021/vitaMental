e146aee3e12fd6c1e2e6cb8642b695788461e54c
Liss <lissj014@gmail.com>
2024-03-20 18:46:55 -0300

    home - login

---
+++ pages/start.js
@@ -0,0 +1,39 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Welcome from '../pages/welcome'
+import SignIn from '../pages/signin'
+import Register from '../pages/register'
+import Home from '../pages/index'
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Welcome"
+                component={Welcome}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="Home"
+                component={Home}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="SignIn"
+                component={SignIn}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="Register"
+                component={Register}
+                options={{ headerShown: false }}
+            />
+
+        </Stack.Navigator>
+    )
+}
--- src/routes/start.js
+++ src/routes/start.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Welcome from '../pages/welcome'
 import SignIn from '../pages/signin'
 import Register from '../pages/register'
+import Home from '../pages/index'
 
 const Stack = createNativeStackNavigator();
 
